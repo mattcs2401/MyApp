e395e2db05916e4c814db50287bf72e682ecd9b0
mattcs2401 <mattcs2401@gmail.com>
2019-04-22 21:27:27 +1000

    Code changes.

---
+++ app/src/main/java/com/mcssoft/myapp/ui/fragment/ToDoEditFragment.kt
@@ -0,0 +1,31 @@
+package com.mcssoft.myapp.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mcssoft.myapp.R
+import kotlinx.android.synthetic.main.toolbar_base.*
+
+class ToDoEditFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_todo_edit, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity?.id_toolbar)?.title = getString(R.string.title_todo) + " TBA"
+
+
+//        // If bottom nav view was previously hidden by a New or Edit etc, then show again.
+//        val bnv = activity?.findViewById(R.id.id_bottom_nav_view) as BottomNavigationView
+//        if(bnv.visibility == View.VISIBLE) {
+//            bnv.visibility = View.GONE
+//        }
+    }
+}
